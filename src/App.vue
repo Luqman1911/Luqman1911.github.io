cc20fa927032ac0396cdf7db79678010c1ec33f7
Luqman1911 <19FTT1197@student.pb.edu.bn>
2022-04-14 11:51:30 +0800

    my profile was added

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/me.jpg" />
+  <h5> Full name: Ak. Luqman Abd. 'Azim Bin Pg. Hj. Samsul-Bahari</h5>
+  <h5>Date of birth: 19th November 1998</h5>
+  <h3> Student of Politeknik</h3>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img{
+  height: 300px;
+  width: 300px;
+}
+</style>
